566e831439bc067cf1d8e1eb3dcb80724b826d9d
Alan Brady <a.brady.3@gmail.com>
2014-10-14 10:05:27 -0700

    Played with formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  #get 'welcome/index'
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'pages#home'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
