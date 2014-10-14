1b5d3fcde1cca433ce97e3cd4801df1f82d24530
Alan Brady <a.brady.3@gmail.com>
2014-10-14 01:03:51 -0700

    Changed root route to welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
+  root 'welcome#index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
