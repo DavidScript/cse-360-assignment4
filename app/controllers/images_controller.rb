cee4fff03b6f7218bd60ed3e96f6cf728d475d63
Alan Brady <a.brady.3@gmail.com>
2014-10-15 03:21:14 -0700

    Finished image uploading infrastructure

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,9 +1,14 @@
 class ImagesController < ApplicationController
 	def new
+		@image = Image.new
 	end
 
 	def create
 		@image = Image.create(user_params)
+		@image.user = current_user
+		@image.save
+	
+		redirect_to root_path
 	end
 	
 	def edit
