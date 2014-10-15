113a8371835bc75aca8e6c0c0a56411152a6dd77
Alan Brady <a.brady.3@gmail.com>
2014-10-14 19:55:20 -0700

    Fixed display_name settings and display

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+
+	private
+
+	def account_update_params
+		params.require(:user).permit(:display_name, :email, :password, :password_confirmation, :current_password)
+	end
+
+end
--- db/migrate/20141015022244_add_display_name.rb
+++ db/migrate/20141015022244_add_display_name.rb
@@ -1,4 +1,5 @@
 class AddDisplayName < ActiveRecord::Migration
   def change
+		add_column :users, :display_name, :string
   end
 end
